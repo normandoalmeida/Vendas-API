08c75a13f9ef6793326b98f2949182c59e7c99f7
normandoalmeida <normandodev@gmail.com>
2024-05-23 10:24:21 -0300

    Create NotEmptyListValidator.java

---
+++ vendas/src/main/java/io/github/normandoalmeida/vendas/validation/constraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,21 @@
+package io.github.normandoalmeida.vendas.validation.constraintvalidation;
+
+import io.github.normandoalmeida.vendas.validation.NotEmptyList;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator<NotEmptyList, List> {
+
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext constraintValidatorContext) {
+        return list != null && !list.isEmpty();
+    }
+
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+
+    }
+
+}
