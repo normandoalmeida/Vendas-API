821abc5e3b4a587e2cf8f64ec0f7ac946dcf0528
normandoalmeida <normandodev@gmail.com>
2024-05-23 10:23:40 -0300

    Create PedidoRepository.java

---
+++ vendas/src/main/java/io/github/normandoalmeida/vendas/domain/repository/PedidoRepository.java
@@ -0,0 +1,15 @@
+package io.github.normandoalmeida.vendas.domain.repository;
+
+import io.github.normandoalmeida.vendas.domain.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    @Query(" select p from Pedido p left join fetch p.itens where p.id =:id ")
+    Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
+
+}
