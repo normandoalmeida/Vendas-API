fd57e5ee7ea81b29b1eb2dda7f39ea6507cdcfac
normandoalmeida <normandodev@gmail.com>
2024-05-23 10:23:53 -0300

    Create ApplicationControllerAdvice.java

---
+++ vendas/src/main/java/io/github/normandoalmeida/vendas/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,40 @@
+package io.github.normandoalmeida.vendas.rest.controller;
+
+import io.github.normandoalmeida.vendas.exception.PedidoNaoEncontradoException;
+import io.github.normandoalmeida.vendas.exception.RegraNegocioException;
+import io.github.normandoalmeida.vendas.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+    @ExceptionHandler(PedidoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrors handlePedidoNotFound(PedidoNaoEncontradoException ex){
+        return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValid(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                .map(erro -> erro.getDefaultMessage()).collect(Collectors.toList());
+        return new  ApiErrors(errors);
+    }
+
+}
