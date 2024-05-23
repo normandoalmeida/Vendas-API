e3d0d74314c5739d6a6082813a1b5028da4713b0
normandoalmeida <normandodev@gmail.com>
2024-05-23 10:24:23 -0300

    Create NotEmptyList.java

---
+++ vendas/src/main/java/io/github/normandoalmeida/vendas/validation/NotEmptyList.java
@@ -0,0 +1,21 @@
+package io.github.normandoalmeida.vendas.validation;
+
+import io.github.normandoalmeida.vendas.validation.constraintvalidation.NotEmptyListValidator;
+import jakarta.persistence.Table;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = NotEmptyListValidator.class)
+public @interface NotEmptyList {
+
+    String message() default "A lista não pode estar vazia";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
