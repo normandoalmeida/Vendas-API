1b3601963c62e18a8c848642fd81e6f6285aa8e1
normandoalmeida <normandodev@gmail.com>
2024-05-23 10:23:29 -0300

    Create Pedido.java

---
+++ vendas/src/main/java/io/github/normandoalmeida/vendas/domain/entity/Pedido.java
@@ -0,0 +1,40 @@
+package io.github.normandoalmeida.vendas.domain.entity;
+
+import io.github.normandoalmeida.vendas.domain.enums.StatusPedido;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pedido_id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @Column(name = "data_pedido")
+    private LocalDate dataPedido;
+
+    @Column(name = "total_pedido")
+    private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusPedido status;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+}
